baf420fea367e81cf9e4cf2887f24caa2f6ff3f0
chang <liuchang85501@gmail.com>
2025-06-25 20:30:02 +0800

    feat: fixed problem

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ function ProjectCard({ title, desc }) {
 
 function Bolb() {
   return (
-    <div className="absolute inset-0 -z-10 overflow-hidden">
+    <div className="fixed inset-0 -z-10 overflow-hidden">
       <div className="absolute top-[-100px] left-[-100px] w-[400px] h-[400px] bg-blue-500 opacity-50 rounded-full blur-[100px]"></div>
       <div className="absolute bottom-[-80px] right-[-80px] w-[300px] h-[300px] bg-cyan-500 opacity-50 rounded-full blur-[100px]"></div>
     </div>
